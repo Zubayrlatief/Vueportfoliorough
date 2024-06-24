80207f6efae8ac753edab6b3eaf479e49cf49766
Zubayr-Latief <zubayrlatie007@gmail.com>
2024-06-24 16:35:27 +0200

    m

---
+++ src/components/MainCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="main-container">
+      <div class="container-one">
+      <div class="profile">
+        <img src="https://i.postimg.cc/ctRFcy3N/IMG-20240103-WA0084.jpg" alt="">
+        <h3>Zubayr Abdullatief</h3>
+        <p>Software Developer</p>   
+    </div>
+    <div class="social-links">
+        <a href="">
+        <div class="link">
+            <img src="" alt="">
+            <h4>Git Hub</h4>
+        </div>
+        <div class="link">
+            <img src="" alt="">
+            <h4>Git Hub</h4>
+        </div>
+        <div class="link">
+            <img src="" alt="">
+            <h4>Git Hub</h4>
+        </div>
+       </a>
+     </div>
+   </div>
+ </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+*{
+    background-image: url('https://i.postimg.cc/dVQNp79n/0c52c00443e2fdebd35d8e47d959958a.jpg');
+}
+
+</style>
